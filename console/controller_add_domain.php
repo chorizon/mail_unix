492fa9fe642b1b175f2db2d2f9b859faa561ccfa
Antonio de la Rosa <webmaster@web-t-sys.com>
2015-10-24 03:56:11 +0200

    Added support for ssh connection api

--- console/controller_add_domain.php
+++ console/controller_add_domain.php
@@ -1,18 +1,13 @@
 <?php
 
 use Chorizon\TheServers\Task;
-use GuzzleHttp\Client;
-
-define('NO_JSON_RETURNED', 1);
-define('NO_AUTHENTICATED', 2);
-define('SCRIPT_NOT_EXISTS_IN_DB', 3);
 
 function add_domainConsole() 
 {
 
     //Make standard class method with this.
 
-    Task::make_simple_petition(['category' => 'mail', 'module' => 'mail_unix', 'script' => 'add_domain']);
+    Task::make_simple_petition_ssh(['category' => 'mail', 'module' => 'mail_unix', 'script' => 'add_domain']);
       
 }
 
